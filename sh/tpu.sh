6105dc987678d8a2998be1ce45c27026fc641969
xxai.art <xxai.art@gmail.com>
2023-08-11 15:52:36 +0800

    ♨

--- sh/tpu.sh
+++ sh/tpu.sh
@@ -5,4 +5,4 @@ cd $DIR
 set -ex
 
 source .direnv/python/bin/activate
-pip install torch_xla
+pip install torch~=2.0.0 https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-2.0-cp38-cp38-linux_x86_64.whl
