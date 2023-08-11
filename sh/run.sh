691ffa9f0330479132de24ae5ce2d318fb0d2f75
xxai.art <xxai.art@gmail.com>
2023-08-11 15:49:12 +0800

    ♨

--- sh/run.sh
+++ sh/run.sh
@@ -8,6 +8,10 @@ source .direnv/python/bin/activate
 set -o allexport
 source .env
 set +o allexport
-CUDA_LIB=/usr/local/cuda-$(nvcc -V | grep -o -E '[0-9]+\.[0-9]+' | head -1)
-export LIBRARY_PATH=$LIBRARY_PATH:$CUDA_LIB/lib64:$CUDA_LIB/compat
+
+if ! [ -x "$(command -v nvcc)" ]; then
+  CUDA_LIB=/usr/local/cuda-$(nvcc -V | grep -o -E '[0-9]+\.[0-9]+' | head -1)
+  export LIBRARY_PATH=$LIBRARY_PATH:$CUDA_LIB/lib64:$CUDA_LIB/compat
+fi
+
 ./main.py
